75889320b2c15b94e81369b7a370382aa41b2abe
Nicolas <nicolas@exemple.com>
2026-07-24 19:37:22 +0200

    Ajout sitemap + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://apv-valence.netlify.app",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
